491737a02cff5681c9d287424dd537f20b7eab46
rjgc <rj.leiria@gmail.com>
2014-07-21 11:48:19 +0100

    Updates

--- app/store/Services_Extrusao.js
+++ app/store/Services_Extrusao.js
@@ -26,7 +26,7 @@ Ext.define('ItalboxCatalog.store.Services_Extrusao', {
                items: [{
                     text: 'Extrusão de Produtos de Alumínio',
                     html: '<p style="">Depois do desenvolvimento da solução com perfil de alumínio extrudido e da execução da ferramenta que lhe dará origem, vemos proceder à extrusão desse perfil.</p>'+
-                        '<p style="">O processo de extrusão inicia-se com o pré-aquecimento (cerca de 450ºC) dos biletes de alumínio que são posteriormente inseridos dentro da prensa de extrusão.</p>'+
+                        '<br><p style="">O processo de extrusão inicia-se com o pré-aquecimento (cerca de 450ºC) dos biletes de alumínio que são posteriormente inseridos dentro da prensa de extrusão.</p>'+
                         '<p style="">Com a ajuda de um pistão, o bilete é pressionado contra um orifício que representa a secção pretendida. Este orifício é executado numa fieira fabricada em aço especial de grande resistência. O conjunto denomina-se matriz e é composto por uma placa e contraplaca.</p>'+
                         '<p style="">O perfil, já com a sua forma definitiva, sai pela parte frontal da prensa, sendo conduzido por um equipamento de arrasto. O processo de arrefecimento do perfil, é acelerado através da convexão forçada de ar, obtendo a sua planimetria final com o auxílio de um processo de estiramento.</p>'+
                         '<p style="">Seguidamente vem a operação de corte das barras de perfil nas medidas solicitadas pelo cliente, geralmente 6.00m, 6.30m e 6.50m de comprimento.</p>'+
--- app/store/Services_Vidro.js
+++ app/store/Services_Vidro.js
@@ -35,19 +35,19 @@ Ext.define('ItalboxCatalog.store.Services_Vidro', {
                 },
                {
                     text: 'Pintura Digital Vitrificada',
-                     html: 'A possibilidade de criar uma fachada em vidro e personalizá-la com uma imagem, uma fotografia ou um desenho, é agora possível, por intermédio de um processo de pintura digital. O processo consiste na deposição da tinta de base cerâmica no vidro, através de uma plotter.'+
+                     html: 'A possibilidade de criar uma fachada em vidro e personalizá-la com uma imagem, uma fotografia ou um desenho, é agora possível, por intermédio de um processo de pintura digital.<br> O processo consiste na deposição da tinta de base cerâmica no vidro, através de uma plotter. '+
                            'Após impressão, o vidro passa pela estufa de secagem e seguidamente pelo forno de têmpera a 750° para a tinta vitrificar. Uma vez fundida no vidro, a tinta mantém-se inalterável à radiação solar e resistente à abrasão, permitindo deste modo a exposição das superfícies pintadas a fatores agressivos sem serem afetadas.'+
-                           'Embora possa ser aplicado em interiores a sua utilização está mais direcionada para o exterior pois permite, juntamente com o vidro apropriado, um melhor controlo solar bem como de graduação da transmissão luminosa sem alteração das cores impressas.'+
-                           'Esta nova tecnologia permite aos projectistas combinar e graduar a intensidade luminosa e o controlo solar em vidros termicamente eficientes num amplo leque de soluções decorativas onde o limite é o imaginário.',
+                           '<br>Embora possa ser aplicado em interiores a sua utilização está mais direcionada para o exterior pois permite, juntamente com o vidro apropriado, um melhor controlo solar bem como de graduação da transmissão luminosa sem alteração das cores impressas.'+
+                           '<br>Esta nova tecnologia permite aos projectistas combinar e graduar a intensidade luminosa e o controlo solar em vidros termicamente eficientes num amplo leque de soluções decorativas onde o limite é o imaginário.',
                     foto: 'imgs/imagem3.jpg',
                     icon: 'icon-apoio',
                     leaf: true
                 },
                 {
                     text: 'Serigrafia',
-                    html: '<p>Quando o objectivo passa por uma repetição de uma imagem ou desenho, o método a utilizar será a serigrafia. Este processo de gravação consiste numa tela micro perfurada onde é reproduzida a imagem pretendida, a qual é sobreposta no vidro. Ao aplicar a tinta de base cerâmica por cima da tela, ela irá passar apenas nos espaços com orifícios ficando retida nas restantes áreas. Depois de receber a tinta, o vidro é levado ao forno de têmpera para que a tinta vitrifique nele.</p>'+
-                          '<p>Os vidros serigrafados têm variadíssimas aplicações: em fachadas, permite para além da parte estética, funcionar como forma de controlo solar, assegurando uma boa visibilidade do interior para o exterior; no interior, desde portas, divisórias, cabines de duche ou até mobiliário, desde que aplicada com o vidro adequado.</p>'+
-                          '<p>Qualquer que seja o motivo pretendido, standard ou personalizado, realizado com uma ou mais cores, aplicado no interior ou no exterior, a inalterabilidade das cores é garantida.</p>',
+                    html: '<p>Quando o objectivo passa por uma repetição de uma imagem ou desenho, o método a utilizar será a serigrafia.<br>Este processo de gravação consiste numa tela micro perfurada onde é reproduzida a imagem pretendida, a qual é sobreposta no vidro. Ao aplicar a tinta de base cerâmica por cima da tela, ela irá passar apenas nos espaços com orifícios ficando retida nas restantes áreas. Depois de receber a tinta, o vidro é levado ao forno de têmpera para que a tinta vitrifique nele.</p>'+
+                          '<br><p>Os vidros serigrafados têm variadíssimas aplicações: em fachadas, permite para além da parte estética, funcionar como forma de controlo solar, assegurando uma boa visibilidade do interior para o exterior; no interior, desde portas, divisórias, cabines de duche ou até mobiliário, desde que aplicada com o vidro adequado.</p>'+
+                          '<br><p>Qualquer que seja o motivo pretendido, standard ou personalizado, realizado com uma ou mais cores, aplicado no interior ou no exterior, a inalterabilidade das cores é garantida.</p>',
                     foto: 'imgs/imagem2.jpg',
                     icon: 'icon-apoio',
                     leaf: true
@@ -55,9 +55,9 @@ Ext.define('ItalboxCatalog.store.Services_Vidro', {
                 {
                     text: 'Opacificação',
                     html: '<p style="text-align: justify;">A opacificação é o método mais rápido e simples quando se pretende pintar um vidro com uma única cor.</p>'+
-                        '<p style="text-align: justify;">Consiste no revestimento total e uniforme de uma das faces do vidro com a tinta.</p>'+
-                        '<p style="text-align: justify;">A fusão da tinta no vidro garante a inalterabilidade das cores pelo que também se recomenda a sua aplicação no exterior.</p>'+
-                        '<p style="text-align: justify;">Os efeitos estéticos conseguidos pela reflexão da luz, realçam a beleza da fachada</p>',
+                        '<br><p style="text-align: justify;">Consiste no revestimento total e uniforme de uma das faces do vidro com a tinta.</p>'+
+                        '<br><p style="text-align: justify;">A fusão da tinta no vidro garante a inalterabilidade das cores pelo que também se recomenda a sua aplicação no exterior.</p>'+
+                        '<br><p style="text-align: justify;">Os efeitos estéticos conseguidos pela reflexão da luz, realçam a beleza da fachada</p>',
                     foto: 'imgs/imagem4.jpg',
                     icon: 'icon-apoio',
                     leaf: true
